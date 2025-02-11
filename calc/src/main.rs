7a784bfa4ecb7c306e979bfefc9fb505b0b71a7d
kenmori <kenjimoritata@gmail.com>
2025-02-11 11:16:30 +0900

    refactor: use struct as Memory

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -1,7 +1,6 @@
 use std::io::stdin;
-
 fn main() {
-    let mut memories: Vec<f64> = vec![0.0; 10];
+    let mut memory = Memory { slots: vec![] };
 
     let mut prev_result: f64 = 0.0;
 
@@ -12,20 +11,22 @@ fn main() {
             break;
         }
 
+        // 空白で分割
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
 
+        // メモリへの書き込み
         let is_memory = tokens[0].starts_with("mem");
         if is_memory && tokens[0].ends_with("+") {
-            add_and_print_memory(&mut memories, tokens[0], prev_result);
+            add_and_print_memory(&mut memory, tokens[0], prev_result);
             continue;
         } else if is_memory && tokens[0].ends_with("-") {
-            add_and_print_memory(&mut memories, tokens[0], -prev_result);
+            add_and_print_memory(&mut memory, tokens[0], -prev_result);
             continue;
         }
 
-        let left = eval_token(tokens[0], &memories);
+        let left = eval_token(tokens[0], &memory);
 
-        let right = eval_token(tokens[2], &memories);
+        let right = eval_token(tokens[2], &memory);
 
         let result = eval_expression(left, tokens[1], right);
         print_value(result);
@@ -33,19 +34,34 @@ fn main() {
     }
 }
 
-fn eval_token(token: &str, memories: &[f64]) -> f64 {
+fn eval_token(token: &str, memory: &Memory) -> f64 {
     if token.starts_with("mem") {
-        let slot_index: usize = token[3..].parse().unwrap();
-        memories[slot_index]
+        let slot_name = &token[3..];
+        for slot in &memory.slots {
+            if slot.0 == slot_name {
+                return slot.1;
+            }
+        }
+        0.0
     } else {
         token.parse().unwrap()
     }
 }
 
-fn add_and_print_memory(memories: &mut [f64], token: &str, prev_result: f64) {
-    let slot_index: usize = token[3..token.len() - 1].parse().unwrap();
-    memories[slot_index] += prev_result;
-    print_value(memories[slot_index]);
+struct Memory {
+    slots: Vec<(String, f64)>,
+}
+
+fn add_and_print_memory(memory: &mut Memory, token: &str, prev_result: f64) {
+    let slot_name = &token[3..token.len() - 1];
+    for slot in memory.slots.iter_mut() {
+        if slot.0 == slot_name {
+            slot.1 += prev_result;
+            print_value(slot.1);
+            return;
+        }
+    }
+    memory.slots.push((slot_name.to_string(), prev_result));
 }
 
 fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
