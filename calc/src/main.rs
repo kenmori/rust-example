c93d13ce442e066bd8c725b710ec3201affca347
kenmori <kenjimoritata@gmail.com>
2025-02-12 21:44:30 +0900

    fix: impl

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -1,6 +1,7 @@
+use std::collections::{hash_map::Entry, HashMap};
 use std::io::stdin;
 fn main() {
-    let mut memory = Memory { slots: vec![] };
+    let mut memory = Memory::new();
 
     let mut prev_result: f64 = 0.0;
 
@@ -17,64 +18,67 @@ fn main() {
         // メモリへの書き込み
         let is_memory = tokens[0].starts_with("mem");
         if is_memory && tokens[0].ends_with("+") {
-            add_and_print_memory(&mut memory, tokens[0], prev_result);
+            memory.add_and_print(tokens[0], prev_result);
             continue;
         } else if is_memory && tokens[0].ends_with("-") {
-            add_and_print_memory(&mut memory, tokens[0], -prev_result);
+            memory.add_and_print(tokens[0], -prev_result);
             continue;
         }
 
-        let left = eval_token(tokens[0], &memory);
+        let left = memory.eval_token(tokens[0]);
 
-        let right = eval_token(tokens[2], &memory);
+        let right = memory.eval_token(tokens[2]);
 
-        let result = eval_expression(left, tokens[1], right);
-        print_value(result);
+        let result = memory.eval_expression(left, tokens[1], right);
+        print_output(result);
         prev_result = result;
     }
 }
 
-fn eval_token(token: &str, memory: &Memory) -> f64 {
-    if token.starts_with("mem") {
-        let slot_name = &token[3..];
-        for slot in &memory.slots {
-            if slot.0 == slot_name {
-                return slot.1;
+struct Memory {
+    slots: HashMap<String, f64>,
+}
+
+impl Memory {
+    fn new() -> Self {
+        Self {
+            slots: HashMap::new(),
+        }
+    }
+    fn add_and_print(&mut self, token: &str, prev_result: f64) {
+        let slot_name = &token[3..token.len() - 1];
+        match self.slots.entry(slot_name.to_string()) {
+            Entry::Occupied(mut entry) => {
+                *entry.get_mut() += prev_result;
+                print_output(*entry.get());
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(prev_result);
+                print_output(prev_result);
             }
         }
-        0.0
-    } else {
-        token.parse().unwrap()
     }
-}
-
-struct Memory {
-    slots: Vec<(String, f64)>,
-}
-
-fn add_and_print_memory(memory: &mut Memory, token: &str, prev_result: f64) {
-    let slot_name = &token[3..token.len() - 1];
-    for slot in memory.slots.iter_mut() {
-        if slot.0 == slot_name {
-            slot.1 += prev_result;
-            print_value(slot.1);
-            return;
+    fn eval_token(&self, token: &str) -> f64 {
+        if token.starts_with("mem") {
+            let slot_name = &token[3..];
+            self.slots.get(slot_name).copied().unwrap_or(0.0)
+        } else {
+            token.parse().unwrap()
         }
     }
-    memory.slots.push((slot_name.to_string(), prev_result));
-}
-
-fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
-    match operator {
-        "+" => left + right,
-        "-" => left - right,
-        "*" => left * right,
-        "/" => left / right,
-        _ => {
-            unreachable!();
+    fn eval_expression(&self, left: f64, operator: &str, right: f64) -> f64 {
+        match operator {
+            "+" => left + right,
+            "-" => left - right,
+            "*" => left * right,
+            "/" => left / right,
+            _ => {
+                unreachable!();
+            }
         }
     }
 }
-fn print_value(result: f64) {
+
+fn print_output(result: f64) {
     println!(" => {}", result);
 }
